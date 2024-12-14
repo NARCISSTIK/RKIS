9edd2fd6c4bb9b2ff4b223fef317c5bed62b17b4
🗲Sturmbannführer🗲 <nikitadatcenkoyt@gmail.com>
2024-12-14 12:41:21 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace H
+namespace H
 {
     internal class Program
     {
+        static double GetSquare(int number)
+        {
+            return Math.Pow(Convert.ToDouble(number),2);
+        } 
+        static void Print(double number)
+        {
+            Console.WriteLine(number.ToString());
+        }
         static void Main(string[] args)
         {
             Print(GetSquare(42));
